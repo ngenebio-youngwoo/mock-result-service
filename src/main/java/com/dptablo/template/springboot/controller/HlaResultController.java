c5c5f49d56029858cf5942d34279f5333a402220
youngwoo-ngenebio <youngwoo.lee@ngenebio.com>
2023-02-06 17:09:14 +0900

    HLA Coverage Plot Chart 데이터 조회 mock 구현

--- src/main/java/com/dptablo/template/springboot/controller/HlaResultController.java
+++ src/main/java/com/dptablo/template/springboot/controller/HlaResultController.java
@@ -3,21 +3,47 @@ package com.dptablo.template.springboot.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ngenebio.msa.vdp.model.ResponseDto;
 import com.ngenebio.msa.vdp.model.hla.chart.BaseVariationPlotChartData;
+import com.ngenebio.msa.vdp.model.hla.chart.CoveragePlotChartData;
+import com.ngenebio.msa.vdp.model.hla.report.HlaStandardReportData;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/result/hla")
 public class HlaResultController {
+
+    @GetMapping
+    @RequestMapping("/report/standard/{runId}")
+    public ResponseEntity<ResponseDto<HlaStandardReportData>> getHlaStandardReportData(
+            @PathVariable("runId") String runId,
+            @RequestParam("sampleIdList") List<String> sampleIdList
+    ) throws IOException {
+        var resource = new ClassPathResource(
+                "data.hla/standard.json", this.getClass().getClassLoader());
+        var jsonString = new String(
+                Files.readAllBytes(resource.getFile().toPath())
+        );
+
+        var objectMapper = new ObjectMapper();
+        var reportData = objectMapper.readValue(jsonString, HlaStandardReportData.class);
+
+        var responseDto = ResponseDto.<HlaStandardReportData>builder()
+                .code(HttpStatus.OK.value())
+                .data(reportData)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(responseDto);
+    }
 
     @GetMapping
     @RequestMapping("/chart/base-variation-plot/{runId}/{sampleId}/{locusName}")
@@ -34,6 +60,28 @@ public class HlaResultController {
         var responseDto = ResponseDto.<BaseVariationPlotChartData>builder()
                 .code(HttpStatus.OK.value())
                 .data(baseVariationPlotChartData)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .body(responseDto);
+    }
+
+    @GetMapping
+    @RequestMapping("/chart/coverage-plot/{runId}/{sampleId}/{locusName}")
+    public ResponseEntity<ResponseDto<CoveragePlotChartData>> getCoveragePlotData() throws IOException {
+        var resource = new ClassPathResource(
+                "data.hla/coveragePlotData.json", this.getClass().getClassLoader());
+        var jsonString = new String(
+                Files.readAllBytes(resource.getFile().toPath())
+        );
+
+        var objectMapper = new ObjectMapper();
+        var coveragePlotChartData = objectMapper.readValue(jsonString, CoveragePlotChartData.class);
+
+        var responseDto = ResponseDto.<CoveragePlotChartData>builder()
+                .code(HttpStatus.OK.value())
+                .data(coveragePlotChartData)
                 .build();
 
         return ResponseEntity.ok()
